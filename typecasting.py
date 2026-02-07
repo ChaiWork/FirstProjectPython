1e78751f7d4c76fbfe25632a996b0093c9adc96f
CHAI <chaichairispum@gmail.com>
2026-02-07 22:53:41 +0800

    Type casting

---
+++ typecasting.py
@@ -0,0 +1,21 @@
+name="bro code"
+name1=""
+
+
+age = 25
+gpa= 3.25
+is_student=True
+
+age = str(age)#tukar int kepada string
+gpa = int(gpa)#tukar float kepada int
+
+print(type(age))
+print(gpa)
+
+name = bool(name)#user enter nama
+
+print(name)
+
+name1 = bool(name1)# user x enter nama
+
+print(name1)
