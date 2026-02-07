3b5cf65ed32169ae8fe5714e2a295267edf50a2d
CHAI <chaichairispum@gmail.com>
2026-02-07 22:18:56 +0800

    Variable

---
+++ variable.py
@@ -0,0 +1,32 @@
+first_name= "Chai"
+food = "Pizza"
+
+#string
+print(first_name)#ambik balik nama
+
+
+print(f"Heloo {first_name}")#string with variable using format string
+
+print(f"Heloo {first_name} u like {food}?" )
+
+
+#integer
+
+age=15
+
+print(f"You are {age}")
+
+#boolean
+
+is_student=True
+
+print(f"Are you a student{is_student}")
+
+
+
+#if else
+
+if is_student:
+    print("You are student")
+else:
+    print("you are not student")
