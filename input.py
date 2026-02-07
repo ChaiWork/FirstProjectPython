4977eaf9ae734e4d93bd2f6efccd580517658c2a
CHAI <chaichairispum@gmail.com>
2026-02-07 23:02:13 +0800

    input

---
+++ input.py
@@ -0,0 +1,12 @@
+
+
+
+name = input("What is Your Name? : ")#akan store input as string
+age = int(input("How old are you? :"))#tukar string kepada int
+
+
+age=age+1 #calculate
+
+print(f"Hello {name}")
+print("Happy Birthday")
+print(f"You are {age} years old")
