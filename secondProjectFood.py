e940932d45cd5da56b5f831d142b5126f6053f9a
CHAI <chaichairispum@gmail.com>
2026-02-08 21:49:01 +0800

    inputExercise

---
+++ secondProjectFood.py
@@ -0,0 +1,7 @@
+item=input("What item would u like to buy? : ")
+price=float(input("What is the price ? : "))
+quantity=int(input("How many would u like ? :"))
+
+total= price*quantity
+
+print(f'Total price of {item} is {total}')
