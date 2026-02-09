02e09030ba8288bc83c7ba12c6cb0e9c54e732fd
CHAI <chaichairispum@gmail.com>
2026-02-09 22:33:33 +0800

    kgtopoundconverter

---
+++ weightConverter.py
@@ -0,0 +1,25 @@
+print("Choice 1 KG TO POUND ")
+print("Choice 2 POUND TO KG ")
+choic=input("Enter your choice")
+result=0
+
+
+
+
+
+
+
+if choic=='1':
+    
+    weightKg = float(input("Enter you Kg to convert in pound"))
+    
+    result=weightKg*2.20462262
+    
+    print(round(result,3))
+    
+elif choic=='2':
+    weightPound=float(input("Enter you Pound to convert in Kg"))
+    
+    result= weightPound/2.20462262
+    
+    print(round(result,3))
