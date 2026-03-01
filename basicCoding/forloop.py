d817e8734ded76779cb382065a437d1153aa4dc7
CHAI <chaichairispum@gmail.com>
2026-03-01 17:17:35 +0800

    forloop

---
+++ basicCoding/forloop.py
@@ -0,0 +1,15 @@
+# range(5) = 0, 1, 2, 3, 4
+for i in range(5):
+    print(f"Number: {i}")
+    
+print()
+
+# range(1, 6) = 1, 2, 3, 4, 5
+for i in range(1, 6):
+    print(f"Number: {i}")
+
+print()
+
+# range(1, 10, 2) = 1, 3, 5, 7, 9 (step of 2)
+for i in range(1, 10, 2):
+    print(f"Odd: {i}")
