1038a1a48137cdff9125373d8c65530e281c31ef
CHAI <chaichairispum@gmail.com>
2026-03-03 17:23:24 +0800

    GameRocKPaperScisor

---
+++ rockpaperscisors.py
@@ -0,0 +1,35 @@
+from ast import If
+import random
+
+options = ("rock","paper","scissors")
+
+running = True
+
+while running:
+    
+    player = None
+    computer=random.choice(options)
+
+    while player not in options:
+        player = input("Enter a choice (Rock papaer scissors) : ")
+
+
+
+    print(f"Player: {player}")
+    print(f"Player: {computer}")
+
+    if player == computer:
+        print("Its a tie")
+    elif player == 'rock' and computer == "scissors":
+        print("You win !!")
+    elif player == 'paper' and computer == "rock":
+        print("You win !!")
+    elif player == 'scissors' and computer == "paper":
+        print("You win !!")
+    else:
+        print("You Lose!!")
+        
+    if not input("Play again? (y/n): ").lower()== 'y':
+        running = False
+
+print("THANKS FOR PLAYING!!!")
