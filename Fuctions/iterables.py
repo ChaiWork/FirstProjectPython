cdb6f4ce148672906d7d9c70c92c4c8152d4f1b2
CHAI <chaichairispum@gmail.com>
2026-03-05 20:42:49 +0800

    iterables

---
+++ Fuctions/iterables.py
@@ -0,0 +1,9 @@
+# iterables = an  object /colelction that can return its 
+# element one at a time, allowing it to be iterated over in a loop
+
+
+
+my_dictiornary = {"A": 1, "B":2 , "C":3}
+
+for key, value in my_dictiornary.items():
+    print(f"{key}={value}")
