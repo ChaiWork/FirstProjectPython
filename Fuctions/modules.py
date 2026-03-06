a0e1417efd66e3b56d2743a11a18a26aed15cb45
CHAI <chaichairispum@gmail.com>
2026-03-06 21:57:21 +0800

    testModules

---
+++ Fuctions/modules.py
@@ -0,0 +1,11 @@
+#module = a file containing code you want to include 
+# in your program use " import" to include a module 
+# (built-in your own) useful to break up a large program 
+# resusable seperate files
+
+import math #use this
+#import math as m
+#from math import pi # could be name conflict very rare to use
+
+print(m.pi)
+print(pi)
